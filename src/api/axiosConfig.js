fedf7b511bdc73b177177d82a8688296763b1dbd
light_dev <dgr8steve95@gmail.com>
2024-11-17 23:06:09 +0100

    updated baseUrl from development localhost to deployed url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,14 +1,14 @@
 // src/api/axiosConfig.js
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL
+  baseURL: "https://peer-2-peer-payment-platfrom-backend.onrender.com", // Replace with your backend URL
 });
 
 // Add a request interceptor to include the token in headers
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token'); // Or get from Redux store
+    const token = localStorage.getItem("token"); // Or get from Redux store
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
